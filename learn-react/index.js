86dc2ae7460e87a233a472397e75b888803bfda9
Joe Junior Nyoni <juniornyoni@gmail.com>
2025-02-17 20:07:31 +0200

    push current tutorial projects

---
+++ learn-react/index.js
@@ -0,0 +1,12 @@
+// ReactDOM.render(
+//   <div>
+//     <h1>React Course</h1>
+//     <p>Today we will be learning React</p>
+//   </div>
+// , document.getElementById("root"));
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<div>
+  <h1>React Course</h1>
+  <p>Today we will be learning React</p>
+</div>);
